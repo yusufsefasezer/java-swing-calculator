9dd7f3666048c8c84367832ba5ba9fa131120367
Yusuf Sezer <yusufsezer@mail.com>
2024-01-31 00:01:44 +0300

    designed GUI

--- src/main/java/com/yusufsezer/controller/CalculatorController.java
+++ src/main/java/com/yusufsezer/controller/CalculatorController.java
@@ -2,6 +2,10 @@ package com.yusufsezer.controller;
 
 import com.yusufsezer.model.CalculatorModel;
 import com.yusufsezer.view.CalculatorView;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CalculatorController {
 
@@ -11,10 +15,82 @@ public class CalculatorController {
     public CalculatorController(CalculatorModel model, CalculatorView view) {
         this.calculatorModel = model;
         this.calculatorView = view;
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints(1, 1, 1, 1, 50D, 10D, GridBagConstraints.CENTER, GridBagConstraints.BOTH, new Insets(1, 1, 1, 1), 1, 1);
+
+        gridBagConstraints.gridwidth = 20;
+        gridBagConstraints.gridx = 1;
+        view.createDisplayText(gridBagConstraints);
+
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        view.makebutton("7", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 5;
+        view.makebutton("8", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 9;
+        view.makebutton("9", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 12;
+        view.makebutton("/", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 3;
+        view.makebutton("4", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 5;
+        view.makebutton("5", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 9;
+        view.makebutton("6", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 12;
+        view.makebutton("*", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 4;
+        view.makebutton("1", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 5;
+        view.makebutton("2", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 9;
+        view.makebutton("3", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 12;
+        view.makebutton("-", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 5;
+        view.makebutton("C", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 5;
+        view.makebutton("0", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 9;
+        view.makebutton("=", gridBagConstraints, new DefaultButtonListener());
+
+        gridBagConstraints.gridx = 12;
+        view.makebutton("+", gridBagConstraints, new DefaultButtonListener());
+
     }
 
     public void showGUI() {
         calculatorView.setVisible(true);
+    }
+
+    private static class DefaultButtonListener implements ActionListener {
+
+        public DefaultButtonListener() {
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
     }
 
 }
--- src/main/java/com/yusufsezer/view/CalculatorView.java
+++ src/main/java/com/yusufsezer/view/CalculatorView.java
@@ -1,14 +1,43 @@
 package com.yusufsezer.view;
 
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class CalculatorView extends JFrame {
 
+    private JTextField displayText;
+
     public CalculatorView() {
         setTitle("Java Swing Calculator");
-        setSize(400, 400);
+        setSize(600, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        setLayout(new GridBagLayout());
+    }
+
+    public void createDisplayText(GridBagConstraints gridBagConstraints) {
+        displayText = new JTextField();
+        displayText.setEditable(false);
+        displayText.setText("Yusuf Sezer");
+        displayText.setHorizontalAlignment(JTextField.RIGHT);
+
+        Font font = new Font("Verdana", Font.PLAIN, 36);
+        displayText.setFont(font);
+        add(displayText, gridBagConstraints);
+    }
+
+    public void makebutton(String buttonText,
+            GridBagConstraints gridBagConstraints,
+            ActionListener actionListener) {
+        JButton newButton = new JButton(buttonText);
+        newButton.addActionListener(actionListener);
+        add(newButton, gridBagConstraints);
     }
 
 }
